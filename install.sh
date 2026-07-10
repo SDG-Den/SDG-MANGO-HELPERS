4278aff334e14ec8f55278d3fb45b4bdeb531fd9
SDGDen <sdgden@sdgcloud.nl>
2026-07-10 12:55:20 +0200

    auto-update

--- install.sh
+++ install.sh
@@ -6,19 +6,12 @@ unipkg install any bat
 unipkg install any jq
 unipkg install any micro
 
-# set working directory
 WORKDIR="$HOME/.cache/SDG-PKG/sdg-mango-helpers"
 
-# install binaries
-cp -r "$WORKDIR/local/"* "$HOME/.local"
+cp -r "$WORKDIR/local/"* "$HOME/.local/"
+cp -r "$WORKDIR/docs/"* "$HOME/.local/docs/"
+cp -r "$WORKDIR/tips/"* "$HOME/.local/tips/"
 
-# install docs and tips
-mkdir -p "$HOME/.local/docs"
-mkdir -p "$HOME/.local/tips"
-cp -r "$WORKDIR/docs/"* "$HOME/.local/docs"
-cp -r "$WORKDIR/tips/"* "$HOME/.local/tips"
-
-# make all scripts executable
 chmod a+x "$HOME/.local/SDG-DMS-BARS/bar-presets.sh"
 chmod a+x "$HOME/.local/SDG-MANGO-LAYOUTS/layout-switch.sh"
 chmod a+x "$HOME/.local/SDG-MANGO-LAYOUTS/layoutbutton.sh"
@@ -29,13 +22,11 @@ chmod a+x "$HOME/.local/SDG-MANGO-CONF/browser.sh"
 chmod a+x "$HOME/.local/SDG-MANGO-SWAP/marker.sh"
 chmod a+x "$HOME/.local/SDG-MANGO-SWAP/swapmarked.sh"
 
-# symlink entrypoints
 sudo ln -sf "$HOME/.local/SDG-DMS-BARS/bar-presets.sh" /usr/bin/dmsbars
 sudo ln -sf "$HOME/.local/SDG-MANGO-LAYOUTS/layout-switch.sh" /usr/bin/manlayout
 sudo ln -sf "$HOME/.local/SDG-MANGO-CONF/mango-config.sh" /usr/bin/manconf
 sudo ln -sf "$HOME/.local/SDG-MANGO-CONF/menu.sh" /usr/bin/manmenu
 
-# verify binaries
 which dmsbars || echo "INSTALL FAILED!"
 which manlayout || echo "INSTALL FAILED!"
 which manconf || echo "INSTALL FAILED!"
--- update.sh
+++ update.sh
@@ -2,20 +2,17 @@
 
 WORKDIR="$HOME/.cache/SDG-PKG/sdg-mango-helpers"
 
-rm -rf "$HOME/.local/SDG-DMS-BARS"
-rm -rf "$HOME/.local/SDG-MANGO-LAYOUTS"
-rm -rf "$HOME/.local/SDG-MANGO-CONF"
-rm -rf "$HOME/.local/SDG-MANGO-SWAP"
-cp -r "$WORKDIR/local/"* "$HOME/.local"
+rm -rf "$HOME/.local/SDG-DMS-BARS" \
+      "$HOME/.local/SDG-MANGO-LAYOUTS" \
+      "$HOME/.local/SDG-MANGO-CONF" \
+      "$HOME/.local/SDG-MANGO-SWAP"
+cp -r "$WORKDIR/local/"* "$HOME/.local/"
+
+rm -rf "$HOME/.local/docs/SDG-MANGO-HELPERS" "$HOME/.local/tips/SDG-MANGO-HELPERS"
+cp -r "$WORKDIR/docs/"* "$HOME/.local/docs/"
+cp -r "$WORKDIR/tips/"* "$HOME/.local/tips/"
 
 sudo ln -sf "$HOME/.local/SDG-DMS-BARS/bar-presets.sh" /usr/bin/dmsbars
 sudo ln -sf "$HOME/.local/SDG-MANGO-LAYOUTS/layout-switch.sh" /usr/bin/manlayout
 sudo ln -sf "$HOME/.local/SDG-MANGO-CONF/mango-config.sh" /usr/bin/manconf
 sudo ln -sf "$HOME/.local/SDG-MANGO-CONF/menu.sh" /usr/bin/manmenu
-
-rm -rf "$HOME/.local/docs/SDG-MANGO-HELPERS"
-rm -rf "$HOME/.local/tips/SDG-MANGO-HELPERS"
-mkdir -p "$HOME/.local/docs"
-mkdir -p "$HOME/.local/tips"
-cp -r "$WORKDIR/docs/"* "$HOME/.local/docs"
-cp -r "$WORKDIR/tips/"* "$HOME/.local/tips"
